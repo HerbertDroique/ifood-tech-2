7f94faa9adfe8140ac9de27649f2f45178a04daf
HerbertDroique <droique15@gmail.com>
2023-10-21 23:05:27 -0300

    segundo commit com funçao criada e if

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("\t\t\t\tCalculadora Rankeada")
+
+
+let saldo = calcularSaldo(20,10)
+
+if (saldo<11){
+    let nome = "Ferro"
+    console.log("Seu nível é: " + saldo, "Seu status é: " +nome)
+
+}
+
+
+
+function calcularSaldo (vitoria, derrota){
+    let resultado = vitoria - derrota
+    return resultado
+
+}
+
+
+    
